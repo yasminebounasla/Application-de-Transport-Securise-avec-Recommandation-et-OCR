d47115a62251e9d24ae2703944ae419cda3c32d3
yasmine bounasla <yasminebounasla@gmail.com>
2026-01-08 18:52:08 +0100

    add routes for auth functions

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { registerDriver, loginDriver } from '../controllers/authDriverController.js';
+import { registerPassenger, loginPassenger } from '../controllers/authPassengerController.js';
+
+const router = express.Router();
+
+// Routes for Driver authentication
+router.post('/driver/register', registerDriver);
+router.post('/driver/login', loginDriver);
+
+router.post('/passenger/register', registerPassenger);
+router.post('/passenger/login', loginPassenger);
