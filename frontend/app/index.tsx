c989d85ac90699a36b4065422289020793e133ef
yasmine bounasla <yasminebounasla@gmail.com>
2026-01-12 19:31:41 +0100

    fix the frontend structure

---
+++ frontend/app/index.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from "react-native";
+
+export default function Index() {   
+    return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Welcome to the Home Page!</Text>
+    </View>
+  );
+}
