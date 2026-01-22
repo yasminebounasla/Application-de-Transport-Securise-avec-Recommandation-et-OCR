f9bb5860608d2b874b27717c759b1f0082be526a
yasmine bounasla <yasminebounasla@gmail.com>
2026-01-22 10:49:56 +0100

    add register forms for both driver and passenger

---
+++ frontend/app/auth/RegisterDriverScreen.tsx
@@ -0,0 +1,63 @@
+import { Button } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
+import { useState } from 'react';
+import { Stack } from '../../.expo/types/router';
+
+export default function RegisterPassengerScreen() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [familyName, setFamilyName] = useState('');
+    const [age, setAge] = useState('');
+    const [sexe, setSexe] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+
+    return (
+        <View>
+        <Stack.Screen options={{ title: 'Register Passenger' }} />
+        <View>
+            <Text>email</Text>
+            <TextInput value={email} onChangeText={setEmail} placeholder='Email'/>
+
+            <Text>password</Text>
+            <TextInput value={password} onChangeText={setPassword} placeholder='Password' secureTextEntry/>
+
+            <Text>confirm password</Text>
+            <TextInput value={confirmPassword} onChangeText={setConfirmPassword} placeholder='Confirm Password' secureTextEntry/>
+
+            <Text>first name</Text>
+            <TextInput value={firstName} onChangeText={setFirstName} placeholder='First Name'/>
+
+            <Text>family name</Text>  
+            <TextInput value={familyName} onChangeText={setFamilyName} placeholder='Family Name'/>
+
+            <Text>age</Text>
+            <TextInput value={age} onChangeText={setAge} placeholder='Age' keyboardType='numeric'/>
+
+            <Text>sexe</Text>
+            <TextInput value={sexe} onChangeText={setSexe} placeholder='Sexe'/>
+
+            <Text>phone number</Text>
+            <TextInput value={phoneNumber} onChangeText={setPhoneNumber} placeholder='Phone Number' keyboardType='phone-pad'/>
+
+            <Text>Register Button</Text>
+            <Button
+                title="Register"
+                onPress={() => {
+                    console.log({
+                    email,
+                    password,
+                    confirmPassword,
+                    firstName,
+                    familyName,
+                    phoneNumber,
+                    });
+                }}
+            />
+
+        </View>
+        </View>
+    );
+}
