98deb5c4df5a72985454624cdc80514cccacd14c
ikramhadjmohamed <ikramhadjmohamed5@gmail.com>
2026-01-28 16:16:23 +0100

    Écran MapScreen partagé

--- frontend/app/shared/MapScreen.tsx
+++ frontend/app/shared/MapScreen.tsx
@@ -1,19 +1,65 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet ,TextInput, TouchableOpacity, Text } from "react-native";
 import MapView from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function MapScreen() {
+  const [location, setLocation] = useState(null);
+  const [searchText, setSearchText] = useState("");
+
+  //Get user location
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        alert("Permission denied");
+        return;
+      }
+      let loc = await Location.getCurrentPositionAsync({});
+      setLocation(loc.coords);
+    })();
+  }, []);
+  // Search address
+  const handleSearch = async () => {
+    if (!searchText) return;
+
+    const result = await Location.geocodeAsync(searchText);
+
+    if (result.length > 0) {
+      setLocation({
+        latitude: result[0].latitude,
+        longitude: result[0].longitude,
+      });
+    }
+  };
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 36.7538,
-          longitude: 3.0588,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
-        }}
-      />
+    <View style={styles.searchBox}>
+        <TextInput
+          placeholder="Search address..."
+          value={searchText}
+          onChangeText={setSearchText}
+          style={styles.input}
+        />
+        <TouchableOpacity onPress={handleSearch} style={styles.searchBtn}>
+          <Text style={{ color: "white" }}>Go</Text>
+        </TouchableOpacity>
+     </View>
+      {location && (
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+          }}
+          showsUserLocation={true}
+          zoomControlEnabled={true}
+        >
+          <Marker coordinate={location} />
+        </MapView>
+      )}
     </View>
   );
 }
@@ -24,4 +70,27 @@ const styles = StyleSheet.create({
   map: {
     flex: 1,
   },
+  searchBox: {
+    position: "absolute",
+    top: 40,
+    left: 10,
+    right: 10,
+    flexDirection: "row",
+    backgroundColor: "white",
+    zIndex: 1,
+    borderRadius: 8,
+    padding: 5,
+  },
+
+  input: {
+    flex: 1,
+    padding: 8,
+  },
+
+  searchBtn: {
+    backgroundColor: "black",
+    padding: 10,
+    borderRadius: 5,
+  },
+
 });
