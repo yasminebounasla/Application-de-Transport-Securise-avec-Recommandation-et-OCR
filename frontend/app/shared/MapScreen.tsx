bf30bd2667c57b768d8b646f464a425e7355cbed
ikramhadjmohamed <ikramhadjmohamed5@gmail.com>
2026-01-28 11:30:47 +0100

    Update packages for Expo Go

--- frontend/app/shared/MapScreen.tsx
+++ frontend/app/shared/MapScreen.tsx
@@ -1,7 +1,27 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
+
 export default function MapScreen() {
   return (
-    <>
-      {/* UI */}
-    </>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 36.7538,
+          longitude: 3.0588,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+      />
+    </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
