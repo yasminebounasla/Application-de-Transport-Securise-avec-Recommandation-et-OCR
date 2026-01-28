6dd69d9aed673fc97934ae68655a8c57c770c00e
ikramhadjmohamed <ikramhadjmohamed5@gmail.com>
2026-01-28 19:37:01 +0100

    Obtenir position GPS actuelle

--- frontend/services/locationService.js
+++ frontend/services/locationService.js
@@ -1,19 +1,1 @@
-import * as Location from 'expo-location';
 
-export async function getCurrentLocation() {
-  let { status } = await Location.requestForegroundPermissionsAsync();
-  if (status !== 'granted') {
-    throw new Error('Permission denied');
-  }
-  let loc = await Location.getCurrentPositionAsync({});
-  return loc.coords; 
-}
-
-export async function geocodeAddress(address) {
-  const result = await Location.geocodeAsync(address);
-  if (result.length === 0) return null;
-  return {
-    latitude: result[0].latitude,
-    longitude: result[0].longitude,
-  };
-}
