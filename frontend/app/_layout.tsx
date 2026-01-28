2a357cca6b1ea3d2ed6729ddc10da779dc434a5b
yasmine bounasla <yasminebounasla@gmail.com>
2026-01-28 10:46:06 +0100

    fix the main file with AuthProvider

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,17 +1,16 @@
 import { Stack } from 'expo-router';
+import { AuthProvider } from '../context/AuthContext';
 import '../global.css';
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ title: 'Home' }}
-      />
-      <Stack.Screen
-        name="auth"
-        options={{ headerShown: false }} 
-      />
-    </Stack>
+    <AuthProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ title: 'Home' }} />
+        <Stack.Screen name="auth" options={{ headerShown: false }} />
+        <Stack.Screen name="driver" options={{ headerShown: false }} />
+        <Stack.Screen name="passenger" options={{ headerShown: false }} />
+      </Stack>
+    </AuthProvider>
   );
 }
