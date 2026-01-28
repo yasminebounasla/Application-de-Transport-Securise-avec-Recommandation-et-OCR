639735835c067321f107ca76bba7e4f5302838ad
ikramhadjmohamed <ikramhadjmohamed5@gmail.com>
2026-01-28 19:16:08 +0100

    Écran MapScreen partagé

--- frontend/app/shared/MapScreen.tsx
+++ frontend/app/shared/MapScreen.tsx
@@ -2,36 +2,35 @@ import React, { useState, useEffect } from "react";
 import { View, StyleSheet ,TextInput, TouchableOpacity, Text } from "react-native";
 import MapView, { Marker }  from "react-native-maps";
 import * as Location from "expo-location";
+import { getCurrentLocation, geocodeAddress } from "../services/locationService"; 
 
 export default function MapScreen() {
   const [location, setLocation] = useState(null);
   const [searchText, setSearchText] = useState("");
 
   //Get user location
-  useEffect(() => {
+ useEffect(() => {
     (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        alert("Permission denied");
-        return;
+      try {
+        const coords = await getCurrentLocation(); 
+        setLocation(coords);
+      } catch (err) {
+        alert(err.message);
       }
-      let loc = await Location.getCurrentPositionAsync({});
-      setLocation(loc.coords);
     })();
   }, []);
   // Search address
   const handleSearch = async () => {
     if (!searchText) return;
-
-    const result = await Location.geocodeAsync(searchText);
-
-    if (result.length > 0) {
-      setLocation({
-        latitude: result[0].latitude,
-        longitude: result[0].longitude,
-      });
+    try {
+      const coords = await geocodeAddress(searchText); // <-- use service
+      if (coords) setLocation(coords);
+      else alert("Address not found");
+    } catch (err) {
+      alert("Error searching address");
     }
   };
+  
   return (
     <View style={styles.container}>
     <View style={styles.searchBox}>
@@ -94,4 +93,5 @@ const styles = StyleSheet.create({
   },
 
 });
+
 
