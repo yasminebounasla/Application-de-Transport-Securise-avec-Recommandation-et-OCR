826dd75ce089274fad1aa5a9a15f67c83c685e0f
ikramhadjmohamed <ikramhadjmohamed5@gmail.com>
2026-01-29 13:02:16 +0100

    Permettre de placer un marqueur sur la carte dans le Composant LocationPicker

---
+++ frontend/components/LocationPicker.js
@@ -0,0 +1,40 @@
+import React, { useContext, useEffect, useRef } from "react";
+import { LocationContext } from "../context/LocationContext";
+import MapView, { Marker } from "react-native-maps";
+
+export default function LocationPicker({ mapRef, searchLocation }) {
+  const { endLocation, setEndLocation } = useContext(LocationContext);
+  const markerRef = useRef(null);
+
+  // Si searchLocation change, déplacer le marker
+  useEffect(() => {
+    if (searchLocation && setEndLocation) {
+      setEndLocation(searchLocation);
+
+      // Centrer la carte sur la nouvelle position
+      if (mapRef && mapRef.current) {
+        mapRef.current.animateToRegion(
+          {
+            latitude: searchLocation.latitude,
+            longitude: searchLocation.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+          },
+          1000
+        );
+      }
+    }
+  }, [searchLocation]);
+
+  if (!endLocation) return null;
+
+  return (
+    <Marker
+      ref={markerRef}
+      coordinate={endLocation}
+      draggable
+      onDragEnd={(e) => setEndLocation(e.nativeEvent.coordinate)}
+      pinColor="black"
+    />
+  );
+}
