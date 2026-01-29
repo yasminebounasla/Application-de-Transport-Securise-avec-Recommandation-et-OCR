670f327a23dce0c1524a8cf224ee07d795e3b2c0
ikramhadjmohamed <ikramhadjmohamed5@gmail.com>
2026-01-29 10:39:28 +0100

    Gérer état global de localisation

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from 'expo-router';
 import { AuthProvider } from '../context/AuthContext';
+import { LocationProvider } from '../context/LocationContext';
 import '../global.css';
 
 export default function Layout() {
   return (
     <AuthProvider>
+      <LocationProvider>
       <Stack>
         <Stack.Screen name="index" options={{ title: 'Home' }} />
         <Stack.Screen name="auth" options={{ headerShown: false }} />
@@ -12,7 +14,7 @@ export default function Layout() {
         <Stack.Screen name="passenger" options={{ headerShown: false }} />
         <Stack.Screen name="shared/MapScreen" options={{ title: 'Map' }} />
       </Stack>
+      </LocationProvider>
     </AuthProvider>
   );
-
 }
