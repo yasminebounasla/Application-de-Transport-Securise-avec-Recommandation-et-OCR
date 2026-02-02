9be09dad00ca6a425ca036c6fd941fe6b790c602
yasmine bounasla <yasminebounasla@gmail.com>
2026-02-02 17:56:13 +0100

    sauvgarder la dataset du light fm avec user et item features

---
+++ ml-service/app.py
@@ -0,0 +1,7 @@
+# API Flask pour recommandations
+
+# Instructions pour exécuter le service de recommandation :
+# cd ml-recommendation-service
+# conda env create -f env.yml
+# conda activate reco-env
+# python app.py
