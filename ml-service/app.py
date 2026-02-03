bbac260c97f613fbc13e090c6c0a8fc9a64da4da
yasmine bounasla <yasminebounasla@gmail.com>
2026-02-03 11:16:10 +0100

    ajouter le serveur flask pour API

--- ml-service/app.py
+++ ml-service/app.py
@@ -1,7 +1,18 @@
-# API Flask pour recommandations
+import os
+from flask import Flask, request, jsonify
+from recommender import get_recommendations
 
-# Instructions pour exécuter le service de recommandation :
-# cd ml-recommendation-service
-# conda env create -f env.yml
-# conda activate reco-env
-# python app.py
+app = Flask(__name__)
+
+@app.route('/recommend', methods=['POST']) 
+def recommend():
+    data = request.get_json()
+    user_id = data.get('user_id')
+    preference = data.get('preference')       
+    
+    recommendations = get_recommendations(user_id, preference)
+    
+    return jsonify({'recommendations': recommendations})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=int(os.getenv("FLASK_PORT", 5000)))
---
+++ ml-service/recommender.py
@@ -0,0 +1,6 @@
+def get_recommendations(user_id, preferences):
+    # appelle le modèle LightFM
+    # prédiction
+    #retourne la liste de drivers
+
+    return ["driver_12", "driver_7", "driver_33"]
