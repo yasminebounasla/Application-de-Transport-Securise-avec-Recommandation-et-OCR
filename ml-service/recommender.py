247fbe5bdc01e048de5e9d9261fc98d8c58291c9
yasmine bounasla <yasminebounasla@gmail.com>
2026-02-04 15:28:12 +0100

    add ML recommendation service with FastAPI and LightFM integration

--- ml-service/recommender.py
+++
@@ -1,7 +0,0 @@
-def get_recommendations(user_id, preferences):
-    # Contient la logique de recommandation
-    # Charge le modèle ML déjà entraîné
-    # Reçoit user_id + préférences
-    # Retourne une liste d’IDs de drivers
-
-    return ["driver_12", "driver_7", "driver_33"]
