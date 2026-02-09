4f824fa645d50d7f087c019d150f0725c470055d
ikramhadjmohamed <ikramhadjmohamed5@gmail.com>
2026-02-09 09:21:05 +0100

    Intégration complète : SearchRideScreen avec Backend

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,19 +1,22 @@
 import { Stack } from 'expo-router';
 import { AuthProvider } from '../context/AuthContext';
 import { LocationProvider } from '../context/LocationContext';
+import { RideProvider } from '../context/RideContext';
 import '../global.css';
 
 export default function Layout() {
   return (
     <AuthProvider>
       <LocationProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ title: 'Home' }} />
-        <Stack.Screen name="auth" options={{ headerShown: false }} />
-        <Stack.Screen name="driver" options={{ headerShown: false }} />
-        <Stack.Screen name="passenger" options={{ headerShown: false }} />
-        <Stack.Screen name="shared/MapScreen" options={{ title: 'Map' }} />
-      </Stack>
+        <RideProvider>
+          <Stack>
+           <Stack.Screen name="index" options={{ title: 'Home' }} />
+           <Stack.Screen name="auth" options={{ headerShown: false }} />
+           <Stack.Screen name="driver" options={{ headerShown: false }} />
+           <Stack.Screen name="passenger" options={{ headerShown: false }} />
+           <Stack.Screen name="shared/MapScreen" options={{ title: 'Map' }} />
+          </Stack>
+        </RideProvider>
       </LocationProvider>
     </AuthProvider>
   );
