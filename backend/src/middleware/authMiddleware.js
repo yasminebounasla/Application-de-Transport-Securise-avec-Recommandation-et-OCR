efa2328365eda12a6c1dd3859cea6e9d8b514809
yasmine bounasla <yasminebounasla@gmail.com>
2026-02-09 08:57:06 +0100

    ajuster le auth middleware avec les nv attributs des token

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -4,25 +4,28 @@ const jwtSecret = process.env.JWT_SECRET;
 
 export const authenticate = (req, res, next) => {
   const authHeader = req.headers.authorization;
-
+  
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "not authorized, missing token" });
+    return res.status(401).json({ message: "Not authorized, missing token" });
   }
-
+  
   const token = authHeader.split(" ")[1];
-
+  
   try {
     const decoded = jwt.verify(token, jwtSecret);
-
-    // on stocke l'ID correspondant selon le type
+    
+    // Ajoute les informations de l'utilisateur décodé à la requête
     req.user = {
-      driverId: decoded.driverId || null,
       passengerId: decoded.passengerId || null,
-      name: decoded.name || null
+      driverId: decoded.driverId || null,
+      name: decoded.name || null,
+      email: decoded.email || null
     };
-
+    
     next();
+    
   } catch (err) {
+    console.error('❌ Token invalide:', err.message);
     return res.status(401).json({ message: "Invalid token" });
   }
 };
