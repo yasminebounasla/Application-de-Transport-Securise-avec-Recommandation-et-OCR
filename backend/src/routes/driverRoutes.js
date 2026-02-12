ff638a3119fb532c8b1e4f53ed7248930035d5ee
yasmine bounasla <yasminebounasla@gmail.com>
2026-02-12 11:18:36 +0100

    dix error

--- backend/src/routes/driverRoutes.js
+++ backend/src/routes/driverRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authenticate } from "../middleware/authMiddleware.js";
-import { addDriverPreferences } from "../controllers/driverController.js";
+import { addDriverPreferences, getDriverRating } from "../controllers/driverController.js";
 
 const router = express.Router();
 
