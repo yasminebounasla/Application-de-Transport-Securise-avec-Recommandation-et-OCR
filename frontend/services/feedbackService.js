d90b272820dc4b975831778146406c86886f91ed
yasmine bounasla <yasminebounasla@gmail.com>
2026-02-15 09:28:08 +0100

    ajouter les focntions de feedback pour le service

--- frontend/services/feedbackService.js
+++ frontend/services/feedbackService.js
@@ -3,6 +3,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const API_URL = "http://192.168.1.69:5000/api";  
 
+// pour les passagers :
+
 // POST feedback
 export const submitFeedback = async (feedback) => {
   try {
@@ -31,13 +33,13 @@ export const submitFeedback = async (feedback) => {
   }
 };
 
-// GET driver rating
-export const getDriverRating = async (driverId) => {
+// GET feedback by trajetId
+export const getFeedbackByTrajet = async (trajetId) => {
   try {
     const token = await AsyncStorage.getItem('token');
 
     const response = await axios.get(
-      `${API_URL}/drivers/${driverId}/rating`,
+      `${API_URL}/feedback/trajet/${trajetId}`,
       {
         headers: {
           'Content-Type': 'application/json',
@@ -49,7 +51,107 @@ export const getDriverRating = async (driverId) => {
 
     return response.data;
   } catch (error) {
-    console.error("Erreur getDriverRating :", error.message);
+    console.error("Erreur getFeedbackByTrajet :", error.message);
     throw error;
   }
 };
+
+// GET public start d'un driver
+export const getPublicDriverStats = async (driverId) => {
+  try {
+    const token = await AsyncStorage.getItem('token');
+
+    const response = await axios.get(
+      `${API_URL}/feedback/driver/${driverId}/stats`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          ...(token && { Authorization: `Bearer ${token}` })
+        },
+        timeout: 30000
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Erreur getPublicDriverStats :", error.message);
+    throw error;
+  }
+};
+
+// GET feedbacks publics d'un driver
+export const getPublicDriverFeedback = async (driverId) => {
+  try {
+    const token = await AsyncStorage.getItem('token');
+
+    const response = await axios.get(
+      `${API_URL}/feedback/driver/${driverId}/public`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          ...(token && { Authorization: `Bearer ${token}` })
+        },
+        timeout: 30000
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Erreur getPublicDriverFeedback :", error.message);
+    throw error;
+  }
+};
+
+
+
+// pour les drivers :
+
+
+// GET stats d'un driver 
+export const getDriverStats = async () => {
+  try {
+    const token = await AsyncStorage.getItem('token');
+
+    const response = await axios.get(
+      `${API_URL}/feedback/my-stats`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          ...(token && { Authorization: `Bearer ${token}` })
+        },
+        timeout: 30000
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Erreur getPublicDriverFeedback :", error.message);
+    throw error;
+  }
+};
+
+
+export const getDriverFeedback = async () => {
+  try {
+    const token = await AsyncStorage.getItem('token');
+
+    const response = await axios.get(
+      `${API_URL}/feedback/my-feedbacks`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          ...(token && { Authorization: `Bearer ${token}` })
+        },
+        timeout: 30000
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Erreur getPublicDriverFeedback :", error.message);
+    throw error;
+  }
+};
+
+
+
