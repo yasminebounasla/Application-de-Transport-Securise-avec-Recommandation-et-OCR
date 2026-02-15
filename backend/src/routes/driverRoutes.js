04a2d762f6d7ce781d4f12820d6be0bc0e01a48c
ikramhadjmohamed <ikramhadjmohamed5@gmail.com>
2026-02-15 10:50:18 +0100

    add API routes pour driver

--- backend/src/routes/driverRoutes.js
+++ backend/src/routes/driverRoutes.js
@@ -4,16 +4,25 @@ import { addDriverPreferences, getDriverRating ,
   addVehicle,
   updateVehicle,
   getDriverVehicles,
-  deleteVehicle } from "../controllers/driverController.js";
+  deleteVehicle ,
+  getDriverPreferences,
+  getDriverProfile,
+  getMyDriverProfile,
+  updateDriverProfile,} from "../controllers/driverController.js";
 
 const router = express.Router();
 
 router.put("/preferences", authenticate, addDriverPreferences);  
 router.get("/:driverId/rating", authenticate, getDriverRating);
+router.get("/preferences", authenticate, getDriverPreferences);
 
 router.post("/vehicle", authenticate, addVehicle);
 router.get("/vehicle", authenticate, getDriverVehicles);
 router.put("/vehicle/:vehicleId", authenticate, updateVehicle);
 router.delete("/vehicle/:vehicleId", authenticate, deleteVehicle);
+
+router.get("/me", authenticate, getMyDriverProfile);
+router.put("/profile", authenticate, updateDriverProfile);
+router.get("/:id", authenticate, getDriverProfile); 
 
 export default router;
