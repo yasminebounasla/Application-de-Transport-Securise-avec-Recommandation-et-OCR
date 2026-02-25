ef82f2366cedaba490057885b903066e21f60948
yasmine bounasla <yasminebounasla@gmail.com>
2026-02-25 11:46:30 +0100

    ajouter le API_URL dans l'env

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const API_URL = 'http://192.168.1.129:3000/api';
+export const API_URL = process.env.EXPO_PUBLIC_API_URL ;
 
 const api = axios.create({
   baseURL: API_URL,
