f83d98771d449037438f2b84da472d901fad6137
Yasmine <yasmine4ever205@gmail.com>
2026-02-27 22:48:18 +0100

    Solve Merging problems

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,13 +1,8 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-<<<<<<< HEAD
 // Use your machine IP and backend port (backend uses PORT 4040 by default)
 export const API_URL = 'http://192.168.1.34:4040/api';
-=======
-
-export const API_URL = process.env.EXPO_PUBLIC_API_URL;  // ajouter cet var au fichier .env
->>>>>>> 17a41a3c1a5e8ae1f360fa8e2049fe9ed18a439e
 
 const api = axios.create({
   baseURL: API_URL,
